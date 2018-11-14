9f08e5b867fddb09e5a5c43bbe9e47408f59eea7
Abhaya Tamrakar <art4829@rit.edu>
2018-11-13 21:32:41 -0500

    Gets data from email and password and shows it on a new page

--- app/src/main/java/com/example/supportme/brick_android/MainActivity.java
+++ app/src/main/java/com/example/supportme/brick_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.supportme.brick_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText email= (EditText) findViewById(R.id.id_email);
+        final EditText password = (EditText)  findViewById(R.id.id_password);
+
+        Button login = (Button) findViewById(R.id.id_logo_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent theIntent = new Intent(MainActivity.this, PrintActivity.class);
+                theIntent.putExtra("email",  email.getText().toString());
+                theIntent.putExtra("password", password.getText().toString());
+                startActivity(theIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/supportme/brick_android/PrintActivity.java
+++ app/src/main/java/com/example/supportme/brick_android/PrintActivity.java
@@ -1,7 +1,9 @@
 package com.example.supportme.brick_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PrintActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class PrintActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print);
+
+        String email = getIntent().getStringExtra("email");
+        String pw = getIntent().getStringExtra("password");
+
+        TextView t = (TextView) findViewById(R.id.textView);
+        TextView t2 = (TextView) findViewById(R.id.textView2);
+
+        t.setText(email);
+        t2.setText(pw);
     }
 }
