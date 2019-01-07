93b310249eb246437467e773b2e118cc6f2521e1
Chris Baudouin, Jr <cjb5326@rit.edu>
2019-01-06 19:52:58 -0500

    - Removes logout button from wristband view - Adds NfcAdapter

--- app/src/main/java/com/example/supportme/brick_android/Wristband.java
+++ app/src/main/java/com/example/supportme/brick_android/Wristband.java
@@ -1,6 +1,7 @@
 package com.example.supportme.brick_android;
 
 import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,12 +10,17 @@ import android.widget.Button;
 
 public class Wristband extends AppCompatActivity {
 
+    NfcAdapter nfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wristband);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
 
         Button history = findViewById(R.id.button_history);
         history.setOnClickListener(new View.OnClickListener() {
