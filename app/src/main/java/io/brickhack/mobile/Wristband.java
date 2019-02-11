77bd835619209ebacbe255935716e15d834a375b
Chris Baudouin, Jr <cjb5326@rit.edu>
2019-02-11 16:34:36 -0500

    - Fixes issue where app would crash if device didn't have NFC and back was pressed

--- app/src/main/java/io/brickhack/mobile/Wristband.java
+++ app/src/main/java/io/brickhack/mobile/Wristband.java
@@ -6,16 +6,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import io.brickhack.mobile.R;
 
 public class Wristband extends AppCompatActivity {
 
@@ -93,7 +91,9 @@ public class Wristband extends AppCompatActivity {
     @Override
     protected void onPause(){
         // Give control back to OS
-        nfcAdapter.disableForegroundDispatch(this);
+        if(nfcAdapter != null && nfcAdapter.isEnabled()) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
         super.onPause();
     }
 }
