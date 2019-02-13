6a2dbf9d1a42290a48d87ecd391db846644a56a3
Chris Baudouin, Jr <cjb5326@rit.edu>
2019-02-13 14:15:48 -0500

    - Adds headers

--- app/src/main/java/io/brickhack/mobile/BrickHackAPI.java
+++ app/src/main/java/io/brickhack/mobile/BrickHackAPI.java
@@ -8,6 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface BrickHackAPI {
@@ -18,6 +19,7 @@ public interface BrickHackAPI {
     @GET("/manage/trackable_tags.json")
     Call<JsonElement> getTags();
 
+    @Headers("Content-Type: application/json")
     @FormUrlEncoded
     @POST("/manage/trackable_events.json")
     Call<JsonElement> submitScan(@Field("trackable-event") JSONObject trackableEvent);
