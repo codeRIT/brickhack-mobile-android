c4411d71e377172b0bb05c7963d5603629c2824d
Chris Baudouin, Jr <cjb5326@rit.edu>
2019-02-13 14:13:16 -0500

    - Adds FormUrlEncoded

--- app/src/main/java/io/brickhack/mobile/BrickHackAPI.java
+++ app/src/main/java/io/brickhack/mobile/BrickHackAPI.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
@@ -17,6 +18,7 @@ public interface BrickHackAPI {
     @GET("/manage/trackable_tags.json")
     Call<JsonElement> getTags();
 
+    @FormUrlEncoded
     @POST("/manage/trackable_events.json")
     Call<JsonElement> submitScan(@Field("trackable-event") JSONObject trackableEvent);
 }
