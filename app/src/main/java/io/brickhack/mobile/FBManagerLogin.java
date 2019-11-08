7283ee591747c76c2f6de46dc0900646aa7abd03
Yoon Kim <yxk3284@g.rit.edu>
2019-11-07 21:12:36 -0500

    added the facebook login for the app

---
+++ app/src/main/java/io/brickhack/mobile/FBManagerLogin.java
@@ -0,0 +1,89 @@
+package io.brickhack.mobile;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.*;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+public class FBManagerLogin extends AppCompatActivity{
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private TextView displayName, emailID;
+    private ImageView displayImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_login);
+        displayName = findViewById(R.id.display_name);
+        emailID = findViewById(R.id.email);
+        displayImage = findViewById(R.id.image_view);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setPermissions(Arrays.asList("email", "public_profile"));
+        // Creating CallbackManager
+        callbackManager = CallbackManager.Factory.create();
+        // Registering CallbackManager with the LoginButton
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // Retrieving access token using the LoginResult
+                AccessToken accessToken = loginResult.getAccessToken();
+                useLoginInformation(accessToken);
+            }
+            @Override
+            public void onCancel() {
+            }
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resulrCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resulrCode, data);
+        super.onActivityResult(requestCode, resulrCode, data);
+    }
+
+    private void useLoginInformation(AccessToken accessToken) {
+        /**
+         Creating the GraphRequest to fetch user details
+         1st Param - AccessToken
+         2nd Param - Callback (which will be invoked once the request is successful)
+         **/
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+            //OnCompleted is invoked once the GraphRequest is successful
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String name = object.getString("name");
+                    String email = object.getString("email");
+                    String image = object.getJSONObject("picture").getJSONObject("data").getString("url");
+                    displayName.setText(name);
+                    emailID.setText(email);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // We set parameters to the GraphRequest using a Bundle.
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,picture.width(200)");
+        request.setParameters(parameters);
+        // Initiate the GraphRequest
+        request.executeAsync();
+
+
+    }
+}
+
+
