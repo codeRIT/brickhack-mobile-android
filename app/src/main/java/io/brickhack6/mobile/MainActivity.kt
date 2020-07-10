ece40c77603704ba67ad73fc2649c2569c76d70f
Emmanuel <aadewale123@gmail.com>
2020-07-09 21:40:59 -0400

    REVAMP Done

---
+++ app/src/main/java/io/brickhack6/mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.brickhack6.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
